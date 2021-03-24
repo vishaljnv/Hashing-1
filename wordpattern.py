4f326529f55a57f8dc9f9236f4d0b241f5060d05
Vishal Yarabandi <blah@blah.com>
2021-03-24 02:30:11 +0000

    Word Pattern complete

---
+++ wordpattern.py
@@ -0,0 +1,23 @@
+def wordPattern(pattern, s):
+    words = s.split()
+    Map = {}
+    wordsSet = set()
+        
+    if len(words) != len(pattern):
+        return False
+    
+    for i in range(len(pattern)):
+        if pattern[i] not in Map:
+            if words[i] in wordsSet:
+                return False
+            Map[pattern[i]] = words[i]
+            wordsSet.add(words[i])
+        if Map[pattern[i]] != words[i]:
+            return False
+    
+    return True
+
+print(wordPattern("abba", "dog cat cat dog"))
+print(wordPattern("abba", "dog cat dog cat"))
+print(wordPattern("", ""))
+
